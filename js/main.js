f57d93df36b82915320e0680dabaded2152aebe6
BugArtur <artur.carv.artur@gmail.com>
2021-09-10 09:04:01 -0300

    primeiro commit do desafio

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var btnInsc = document.getElementById('inscreverse');
+
+btnInsc.addEventListener("click", ()=>{
+    btnInsc.href
+})
+function abregoogle(){
+    window.open('https://web.digitalinnovation.one/course/introducao-criacao-de-websites-com-html5-e-css3/learning/462f831d-5fdf-485e-bf07-1d391eb94ac8/?back=/browse');
+}
